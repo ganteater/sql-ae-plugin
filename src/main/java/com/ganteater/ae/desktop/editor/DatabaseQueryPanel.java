4cafa3d51b8aa03dc9fdd6c3cd69b0d08ab78ef7
Viktor Tovstyi <viktor.tovstyi@gmail.com>
2025-11-18 13:45:02 +0200

    optimized import.

--- src/main/java/com/ganteater/ae/desktop/editor/DatabaseQueryPanel.java
+++ src/main/java/com/ganteater/ae/desktop/editor/DatabaseQueryPanel.java
@@ -6,7 +6,6 @@ import java.awt.Point;
 import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
